31d062ccf6a14d644393e4cdf4ba9c826fa450c8
Rotem <rotema70@gmail.com>
2018-11-28 22:40:27 +0200

    tweet cleaning

--- CleaningTweetsController.py
+++ CleaningTweetsController.py
@@ -1,12 +1,29 @@
 import Tweet
 import Episode
+import datetime
 
 class CleaningTweetsController:
-    episodes = list()
+   episodes = list()
+    #CALL DB TO ADD EPISODES TO THE VAR
 
-def clean_tweet(self, tweet, episode):
+   for episode in episodes:
+        date = episode.date.split("/")
+        episode.date = datetime.datetime(date[2], date[1], date[0])
 
-    for i in range(7):
-        self.episodes.append(episode.date)
 
+def Clean_Tweet(tweet):
 
+    date = tweet.date.split("/")
+    tweet.date = datetime.datetime(date[2], date[1], date[0])
+
+    for i in range(0,8):
+        if tweet.date <= CleaningTweetsController.episodes[0]:
+            tweet.episodeID = CleaningTweetsController.episodes[0]
+
+        elif tweet.date <= CleaningTweetsController.episodes[i]:
+            tweet.episodeID = CleaningTweetsController.episodes[i-1]
+
+        elif tweet.date > CleaningTweetsController.episodes[7]:
+            tweet.episodeID = CleaningTweetsController.episodes[7]
+
+    
